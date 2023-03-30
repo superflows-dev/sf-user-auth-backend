90978a39c8075cdf73ab672b55138e337a93bdae
Hrushi M <108924653+superflows-dev@users.noreply.github.com>
2023-03-30 16:03:11 +0530

    added userid generation logic to validate

--- aws/auth/validate.js
+++ aws/auth/validate.js
@@ -5,6 +5,24 @@ import { generateOTP, generateToken } from './util.js';
 import { ACCESS_TOKEN_DURATION, REFRESH_TOKEN_DURATION } from './globals.js'
 import { processAddLog } from './addLog.js'
 
+export const newUuidV4 = () => {
+  
+  var d = new Date().getTime();//Timestamp
+  var d2 = 0;//Time in microseconds since page-load or 0 if unsupported
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16;//random number between 0 and 16
+      if(d > 0){//Use timestamp until depleted
+          r = (d + r)%16 | 0;
+          d = Math.floor(d/16);
+      } else {//Use microseconds since page-load if supported
+          r = (d2 + r)%16 | 0;
+          d2 = Math.floor(d2/16);
+      }
+      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+
+}
+
 export const processValidate = async (event) => {
     
     
@@ -100,9 +118,47 @@ export const processValidate = async (event) => {
       //processAddLog(email, 'validate', event, response, response.statusCode)
       return response;
     }
+
+    var newUuid = "";
+    
+    if(resultGet.Item.userId == null) {
+      
+      newUuid = newUuidV4();
+      
+      var updateParams = {
+          TableName: TABLE_NAME,
+          Key: {
+            email: { S: email },
+          },
+          UpdateExpression: "set #userId = :userId1",
+          ExpressionAttributeValues: {
+              ":userId1": {"S": newUuid}
+          },
+          ExpressionAttributeNames: {
+              "#userId1": "userId"
+          }
+      };
+      
+      const ddbUpdate = async () => {
+          try {
+            const data = await ddbClient.send(new UpdateItemCommand(updateParams));
+            return data;
+          } catch (err) {
+            console.log(err)
+            return err;
+          }
+      };
+      
+      var resultUpdate = await ddbUpdate();
+      
+    } else {
+      
+      newUuid = resultGet.Item.userId.S;
+      
+    }
     
     if(foundAccessToken && validAccessToken) {
-      const response = {statusCode: 200, body: {result: true, admin: resultGet.Item.admin != null ? resultGet.Item.admin : false}};
+      const response = {statusCode: 200, body: {result: true, userId: newUuid, admin: resultGet.Item.admin != null ? resultGet.Item.admin : false}};
       //processAddLog(email, 'validate', event, response, response.statusCode)
       return response;
     }
